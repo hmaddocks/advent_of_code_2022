cca9552f583ce4e6f06cc48ce22582e242adc613
Henry Maddocks <hmaddocks@me.com>
2022-12-03 17:50:04 +1300

    Clean up Rust Day 2

--- day_2/day_2/src/main.rs
+++ day_2/day_2/src/main.rs
@@ -16,24 +16,20 @@ fn part_1() -> i32 {
         ("C Y".to_owned(), 0),
     ]);
 
-    let file = if let Ok(file) = File::open("../rps.txt") {
-        file
-    } else {
-        todo!()
-    };
-
-    let lines = io::BufReader::new(file).lines();
-    let score = lines.flatten().fold(0, |acc, line| {
-        let us = line.split(' ').last().unwrap();
-        let x = u[&us];
-        let y = g[&line];
-        acc + x + y
-    });
+    let mut score = 0;
+    if let Ok(file) = File::open("../rps.txt") {
+        let lines = io::BufReader::new(file).lines();
+        score = lines.flatten().fold(0, |acc, line| {
+            let us = line.split(' ').last().unwrap();
+            let x = u[&us];
+            let y = g[&line];
+            acc + x + y
+        })
+    }
     score
 }
 
 fn part_2() -> i32 {
-    // let u = HashMap::from([("X", 1), ("Y", 2), ("Z", 3)]);
     let g = HashMap::from([
         ("A X".to_owned(), (0, 3)),
         ("A Z".to_owned(), (6, 2)),
@@ -46,19 +42,14 @@ fn part_2() -> i32 {
         ("C Y".to_owned(), (3, 3)),
     ]);
 
-    let file = if let Ok(file) = File::open("../rps.txt") {
-        file
-    } else {
-        todo!()
-    };
-
-    let lines = io::BufReader::new(file).lines();
-    let score = lines.flatten().fold(0, |acc, line| {
-        // let us = line.split(' ').last().unwrap();
-        // let x = u[&us];
-        let (x, y) = g[&line];
-        acc + x + y
-    });
+    let mut score = 0;
+    if let Ok(file) = File::open("../rps.txt") {
+        let lines = io::BufReader::new(file).lines();
+        score = lines.flatten().fold(0, |acc, line| {
+            let (x, y) = g[&line];
+            acc + x + y
+        })
+    }
     score
 }
 
