47660e5e260d770cff7d0469152c8a527a562e4c
Henry Maddocks <hmaddocks@me.com>
2022-12-03 17:17:32 +1300

    Ruby part 2

--- day_1/day_1.rb
+++ day_1/day_1.rb
@@ -4,3 +4,9 @@ p File.readlines('calories.txt', chomp: true)
       .map(&:sum)
       .max
 
+p File.readlines('calories.txt', chomp: true)
+      .slice_after { |v| v.strip.empty? }
+      .map { |arr| arr.map(&:to_i) }
+      .map(&:sum)
+      .sort[-3..-1]
+      .sum
