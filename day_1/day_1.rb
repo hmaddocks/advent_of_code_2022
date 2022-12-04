75045f0baea62ecf3bf1303882cf778c095d1c31
Henry Maddocks <hmaddocks@me.com>
2022-12-04 18:02:17 +1300

    Code gardening

--- day_1/day_1.rb
+++ day_1/day_1.rb
@@ -8,5 +8,5 @@ p File.readlines('calories.txt', chomp: true)
       .slice_after { |line| line.strip.empty? }
       .map { |group| group.map(&:to_i) }
       .map(&:sum)
-      .sort[-3..-1]
+      .sort[-3..]
       .sum
