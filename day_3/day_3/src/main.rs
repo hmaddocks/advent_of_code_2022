ed5346863a8dcd7d558f7d13494de023bb73085d
Henry Maddocks <hmaddocks@me.com>
2022-12-04 16:23:00 +1300

    Finished Rust Day 3 part 1

--- day_3/day_3/src/main.rs
+++ day_3/day_3/src/main.rs
@@ -1,3 +1,4 @@
+use array_tool::vec::Intersect;
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, BufRead};
@@ -5,26 +6,19 @@ use std::io::{self, BufRead};
 fn main() {
     if let Ok(file) = File::open("../rucksack.txt") {
         let lines = io::BufReader::new(file).lines();
-
         let score = lines.flatten().fold(0, |acc, line| {
-            let left = &line[..(line.len()) / 2 - 1];
+            let left = &line[..(line.len()) / 2];
             let right = &line[line.len() / 2..];
 
-            let left_set = left.chars().collect::<HashSet<_>>();
-            let right_set = right.chars().collect::<HashSet<_>>();
+            let intersection = left
+                .chars()
+                .collect::<Vec<_>>()
+                .intersect(right.chars().collect::<Vec<_>>());
 
-            let c = left_set.intersection(&right_set).collect::<Vec<_>>();
-            // let d = *c as u32;
-            dbg!(c);
-            // acc += c.len()
-            2
+            let item = intersection[0] as u32;
+            acc + if item > 90 { item - 96 } else { item - 38 }
         });
 
         dbg!(score);
     }
-    println!("Hello, world!");
 }
-
-//     l, r = line[..(line.length / 2) - 1], line[line.length / 2..]
-// duplicate_item = (l.chars & r.chars).first
-// acc += duplicate_item.ord > 90 ? duplicate_item.ord - 96 : duplicate_item.ord - 38
