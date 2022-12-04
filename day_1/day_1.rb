c5864f76195434fa8f30695b970b2ef90e649203
Henry Maddocks <hmaddocks@me.com>
2022-12-04 16:56:47 +1300

    Refactor Ruby day 1

--- day_1/day_1.rb
+++ day_1/day_1.rb
@@ -1,12 +1,12 @@
 p File.readlines('calories.txt', chomp: true)
-      .slice_after { |v| v.strip.empty? }
-      .map { |arr| arr.map(&:to_i) }
+      .slice_after { |line| line.strip.empty? }
+      .map { |group| group.map(&:to_i) }
       .map(&:sum)
       .max
 
 p File.readlines('calories.txt', chomp: true)
-      .slice_after { |v| v.strip.empty? }
-      .map { |arr| arr.map(&:to_i) }
+      .slice_after { |line| line.strip.empty? }
+      .map { |group| group.map(&:to_i) }
       .map(&:sum)
       .sort[-3..-1]
       .sum
