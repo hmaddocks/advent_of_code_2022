7bcab94ea9b02e99a54866124bbc827ec56726d9
Henry Maddocks <hmaddocks@me.com>
2022-12-04 16:57:06 +1300

    Elixir day 1 part 2

--- day_1/day_1.exs
+++ day_1/day_1.exs
@@ -1,7 +1,17 @@
-File.read!('calories.txt') \
-|> String.split("\n\n", trim: true) \
-|> Enum.map(fn group -> group |> String.split("\n", trim: true) end) \
-|> Enum.map(fn item -> item |> Enum.map(fn item -> item |> String.to_integer end) end) \
-|> Enum.map(&Enum.sum/1) \
+File.read!('calories.txt')
+|> String.split("\n\n", trim: true)
+|> Enum.map(fn group -> group |> String.split("\n", trim: true) end)
+|> Enum.map(fn item -> item |> Enum.map(fn item -> item |> String.to_integer end) end)
+|> Enum.map(&Enum.sum/1)
 |> Enum.max
+|> IO.puts
 
+File.read!('calories.txt')
+|> String.split("\n\n", trim: true)
+|> Enum.map(fn group -> group |> String.split("\n", trim: true) end)
+|> Enum.map(fn item -> item |> Enum.map(fn item -> item |> String.to_integer end) end)
+|> Enum.map(&Enum.sum/1)
+|> Enum.sort
+|> Enum.slice(-3, 3)
+|> Enum.sum
+|> IO.puts
