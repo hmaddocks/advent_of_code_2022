6b51e06428f8830c41e60923769711105121fdc8
Henry Maddocks <hmaddocks@me.com>
2022-12-06 10:36:37 +1300

    Refactor Ruby day 5

--- day_5/day_5.rb
+++ day_5/day_5.rb
@@ -1,12 +1,15 @@
 input = File.readlines('stacks.txt', chomp: true)
 stack_map = input[0..7]
 
-stacks = stack_map.collect do |row|
-  row.match(/.(.). .(.). .(.). .(.). .(.). .(.). .(.). .(.).? ?.?(.)?.?/).captures
-end.transpose.map do |stack|
-  stack.each.select { |crate| !crate.nil? && crate != ' ' }
+def build_stacks(stack_map)
+  stack_map.collect do |row|
+    row.match(/.(.). .(.). .(.). .(.). .(.). .(.). .(.). .(.).? ?.?(.)?.?/).captures
+  end.transpose.map do |stack|
+    stack.each.select { |crate| !crate.nil? && crate != ' ' }
+  end
 end
 
+stacks = build_stacks(stack_map)
 commands = input[10..].collect do |command|
   m = command.match(/move (\d+) from (\d+) to (\d+)/)
   { take: m[1].to_i, from: m[2].to_i - 1, to: m[3].to_i - 1 }
@@ -21,12 +24,7 @@ end
 top_crates = stacks.map(&:shift).join
 p top_crates # FJSRQCFTN
 
-stacks = stack_map.collect do |row|
-  row.match(/.(.). .(.). .(.). .(.). .(.). .(.). .(.). .(.).? ?.?(.)?.?/).captures
-end.transpose.map do |stack|
-  stack.each.select { |crate| !crate.nil? && crate != ' ' }
-end
-
+stacks = build_stacks(stack_map)
 commands.each do |command|
   stacks[command[:to]].unshift(stacks[command[:from]].shift(command[:take]))
   stacks[command[:to]].flatten!
