1ecadb1c7861d3e8fd9568b7de92efe894851b6e
Henry Maddocks <hmaddocks@me.com>
2022-12-19 21:46:04 +1300

    Clean up the Elixir versions

--- day_1/day_1.exs
+++ day_1/day_1.exs
@@ -1,17 +1,27 @@
-File.read!('calories.txt')
-|> String.split("\n\n", trim: true)
-|> Enum.map(fn group -> group |> String.split("\n", trim: true) end)
-|> Enum.map(fn item -> item |> Enum.map(&String.to_integer/1) end)
-|> Enum.map(&Enum.sum/1)
-|> Enum.max()
+defmodule Day1 do
+  def part_1 do
+    File.read!('calories.txt')
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(fn group -> group |> String.split("\n", trim: true) end)
+    |> Enum.map(fn item -> item |> Enum.map(&String.to_integer/1) end)
+    |> Enum.map(&Enum.sum/1)
+    |> Enum.max()
+  end
+
+  def part_2 do
+    File.read!('calories.txt')
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(fn group -> group |> String.split("\n", trim: true) end)
+    |> Enum.map(fn item -> item |> Enum.map(&String.to_integer/1) end)
+    |> Enum.map(&Enum.sum/1)
+    |> Enum.sort()
+    |> Enum.slice(-3, 3)
+    |> Enum.sum()
+  end
+end
+
+Day1.part_1()
 |> IO.puts()
 
-File.read!('calories.txt')
-|> String.split("\n\n", trim: true)
-|> Enum.map(fn group -> group |> String.split("\n", trim: true) end)
-|> Enum.map(fn item -> item |> Enum.map(&String.to_integer/1) end)
-|> Enum.map(&Enum.sum/1)
-|> Enum.sort()
-|> Enum.slice(-3, 3)
-|> Enum.sum()
+Day1.part_2()
 |> IO.puts()
--- day_2/day_2.exs
+++ day_2/day_2.exs
@@ -1,39 +1,49 @@
-File.read!('rps.txt')
-|> String.split("\n", trim: true)
-|> Enum.reduce(0, fn turn, acc ->
-  [g, h] =
-    %{
-      "A X" => [3, 1],
-      "A Z" => [0, 3],
-      "A Y" => [6, 2],
-      "B X" => [0, 1],
-      "B Z" => [6, 3],
-      "B Y" => [3, 2],
-      "C X" => [6, 1],
-      "C Z" => [3, 3],
-      "C Y" => [0, 2]
-    }[turn]
+defmodule Day2 do
+  def part_1 do
+    File.read!('rps.txt')
+    |> String.split("\n", trim: true)
+    |> Enum.reduce(0, fn turn, acc ->
+      [g, h] =
+        %{
+          "A X" => [3, 1],
+          "A Z" => [0, 3],
+          "A Y" => [6, 2],
+          "B X" => [0, 1],
+          "B Z" => [6, 3],
+          "B Y" => [3, 2],
+          "C X" => [6, 1],
+          "C Z" => [3, 3],
+          "C Y" => [0, 2]
+        }[turn]
 
-  acc + g + h
-end)
+      acc + g + h
+    end)
+  end
+
+  def part_2 do
+    File.read!('rps.txt')
+    |> String.split("\n", trim: true)
+    |> Enum.reduce(0, fn turn, acc ->
+      [g, h] =
+        %{
+          "A X" => [0, 3],
+          "A Z" => [6, 2],
+          "A Y" => [3, 1],
+          "B X" => [0, 1],
+          "B Z" => [6, 3],
+          "B Y" => [3, 2],
+          "C X" => [0, 2],
+          "C Z" => [6, 1],
+          "C Y" => [3, 3]
+        }[turn]
+
+      acc + g + h
+    end)
+  end
+end
+
+Day2.part_1()
 |> IO.puts()
 
-File.read!('rps.txt')
-|> String.split("\n", trim: true)
-|> Enum.reduce(0, fn turn, acc ->
-  [g, h] =
-    %{
-      "A X" => [0, 3],
-      "A Z" => [6, 2],
-      "A Y" => [3, 1],
-      "B X" => [0, 1],
-      "B Z" => [6, 3],
-      "B Y" => [3, 2],
-      "C X" => [0, 2],
-      "C Z" => [6, 1],
-      "C Y" => [3, 3]
-    }[turn]
-
-  acc + g + h
-end)
+Day2.part_2()
 |> IO.puts()
