5a68b4d8811b69dd57db3a1616fdcf420f28ea0b
Henry Maddocks <hmaddocks@me.com>
2022-12-28 13:02:26 +1300

    Day 8 Ruby

--- day_8/day_8.rb
+++ day_8/day_8.rb
@@ -1,15 +1,15 @@
-# input = File.readlines('input.txt', chomp: true)
-input = [
-  "30373",
-  "25512",
-  "65332",
-  "33549",
-  "35390"
-].collect do |row|
-  row.chars
+input = File.readlines('input.txt', chomp: true).collect do |row|
+  row.chars.map(&:to_i)
 end
-
-pp input
+# input = [
+#   "30373",
+#   "25512",
+#   "65332",
+#   "33549",
+#   "35390"
+# ].collect do |row|
+#   row.chars.map(&:to_i)
+# end
 
 height = input.length
 width = input[0].length
@@ -20,44 +20,104 @@ map = [Array.new(width, 1)]
 end
 map << Array.new(width, 1)
 
-def check_top_left(input, x, y, map)
-  p input[y][x]
-end
+def part_one(map, input)
+  height = input.length
+  width = input[0].length
 
-def check_top_right(input, x, y), map
-  p input[y][x]
-end
-
-def check_bottom_left(input, x, y, map)
-  p input[y][x]
-end
-
-def check_bottom_right(input, x, y, map)
-  p input[y][x]
-end
-
-count = 0
-map.each_with_index do |row, y|
-  next if y == 0 || y == height - 1
-
-  row.each_with_index do |tree, x|
-    next if x == 0 || x == width - 1
-
-    if y < height / 2
-      if x < width / 2
-        check_top_left(input, x, y, map)
-      else
-        check_top_right(input, x, y, map)
+  (1...height - 1).to_a.each do |y|
+    highest = input[y][0]
+    (1...width - 1).to_a.each do |x|
+      if input[y][x] > highest
+        highest = input[y][x]
+        map[y][x] = 1
       end
-    else
-      if x < width / 2
-        check_bottom_left(input, x, y, map)
-      else
-        check_bottom_right(input, x, y, map)
+    end
+  end
+
+  (1...height - 1).to_a.each do |y|
+    highest = input[y][width - 1]
+    (1...width - 1).to_a.reverse.each do |x|
+      if input[y][x] > highest
+        highest = input[y][x]
+        map[y][x] = 1
       end
+    end
+  end
+
+  (1...width - 1).to_a.each do |x|
+    highest = input[0][x]
+    (1...height - 1).to_a.each do |y|
+      if input[y][x] > highest
+        highest = input[y][x]
+        map[y][x] = 1
+      end
+    end
+  end
+
+  (1...width - 1).to_a.each do |x|
+    highest = input[height - 1][x]
+    (1...height - 1).to_a.reverse.each do |y|
+      if input[y][x] > highest
+        highest = input[y][x]
+        map[y][x] = 1
+      end
+    end
+  end
+
+  map.reduce(0) do |acc, row|
+    acc += row.reduce(0) do |acc, tree|
+      acc += tree
     end
   end
 end
 
-require 'pp'
-pp map
+pp part_one(map, input)
+
+def score(input, tree_height, x, y, debug = false)
+  height = input.length
+  width = input[0].length
+  p "tree_height #{tree_height}" if debug
+  right = 0
+  p "right" if debug
+  (x...width - 1).to_a.each do |x|
+    p "#{x + 1}, #{y}" if debug
+    right += 1
+    break if input[y][x + 1] >= tree_height
+  end
+
+  left = 0
+  p "left" if debug
+  (1..x).to_a.reverse.each do |x|
+    p "#{x - 1}, #{y}" if debug
+    left += 1
+    break if input[y][x - 1] >= tree_height
+  end
+
+  down = 0
+  p "down" if debug
+  (y...height - 1).to_a.each do |y|
+    p "#{x}, #{y + 1}" if debug
+    down += 1
+    break if input[y + 1][x] >= tree_height
+  end
+
+  up = 0
+  p "up" if debug
+  (1..y).to_a.reverse.each do |y|
+    p "#{x}, #{y - 1}" if debug
+    up += 1
+    break if input[y - 1][x] >= tree_height
+  end
+
+  up * down * left * right
+end
+
+def part_two(input)
+  input.collect.with_index do |row, y|
+    row.collect.with_index do |tree, x|
+      score(input, tree, x, y)
+    end
+  end
+end
+
+pp part_two(input).flatten.max
