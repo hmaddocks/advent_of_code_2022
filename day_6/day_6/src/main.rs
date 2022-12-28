05870e6fbf3a58c00da90f55ac68119ecbc9552a
Henry Maddocks <hmaddocks@me.com>
2022-12-28 21:32:51 +1300

    Rust day 6 part 2

--- day_6/day_6/src/main.rs
+++ day_6/day_6/src/main.rs
@@ -1,21 +1,34 @@
 use itertools::Itertools;
 use std::fs;
 
+fn find_unique_stream(input: &[u8], window_size: usize) -> u32 {
+    match String::from_utf8_lossy(input)
+        .as_bytes()
+        .windows(window_size)
+        .position(|window| window.iter().unique().count() == window_size)
+        .map(|pos| pos + window_size)
+    {
+        Some(pos) => pos.try_into().unwrap(),
+        None => 0,
+    }
+}
+
+fn part_1(input: &[u8]) {
+    let position = find_unique_stream(input, 4);
+    println!("{}", position)
+}
+
+fn part_2(input: &[u8]) {
+    let position = find_unique_stream(input, 14);
+    println!("{}", position)
+}
+
 fn main() {
     match &fs::read("../signal.txt") {
         Ok(input) => {
-            match String::from_utf8_lossy(input)
-                .as_bytes()
-                .windows(4)
-                .position(|window| window.iter().unique().count() == 4)
-                .map(|pos| pos + 4)
-            {
-                Some(pos) => println!("{}", pos),
-                None => println!("None"),
-            };
+            part_1(input);
+            part_2(input);
         }
         Err(e) => println!("{}", e),
     };
-
-    println!("Hello, world!");
 }
