38af12a9ea6221c72ad33d2f80b30884736b17bf
Henry Maddocks <hmaddocks@me.com>
2023-01-04 09:02:26 +1300

    Refactor Elixir day 6

--- day_6/day_6.exs
+++ day_6/day_6.exs
@@ -1,24 +1,29 @@
 defmodule Day6 do
   def part_1 do
-    File.read!('signal.txt')
-    |> String.to_charlist()
-    |> Enum.chunk_every(4, 1)
-    |> Enum.with_index()
-    |> Enum.find(fn {chunk, _} -> Enum.uniq(chunk) == chunk end)
+    {_, index} =
+      File.read!('signal.txt')
+      |> String.to_charlist()
+      |> Enum.chunk_every(4, 1)
+      |> Enum.with_index()
+      |> Enum.find(fn {chunk, _} -> Enum.uniq(chunk) == chunk end)
+
+    index + 4
   end
 
   def part_2 do
-    File.read!('signal.txt')
-    |> String.to_charlist()
-    |> Enum.chunk_every(14, 1)
-    |> Enum.with_index()
-    |> Enum.find(fn {chunk, _} -> Enum.uniq(chunk) == chunk end)
+    {_, index} =
+      File.read!('signal.txt')
+      |> String.to_charlist()
+      |> Enum.chunk_every(14, 1)
+      |> Enum.with_index()
+      |> Enum.find(fn {chunk, _} -> Enum.uniq(chunk) == chunk end)
+
+    index + 14
   end
 end
 
-{chunk, index} = Day6.part_1()
-IO.puts("#{chunk} #{index + 4}")
+Day6.part_1()
+|> IO.puts()
 
-{chunk, index} = Day6.part_2()
-IO.puts("#{chunk} #{index + 14}")
-
+Day6.part_2()
+|> IO.puts()
