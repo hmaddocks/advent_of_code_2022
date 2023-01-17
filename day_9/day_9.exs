217f40f91346f089fdc6331797772c3d0b950220
Henry Maddocks <hmaddocks@me.com>
2023-01-17 16:50:28 +1300

    Day 9 Elixir part 1 works

--- day_9/day_9.exs
+++ day_9/day_9.exs
@@ -1,59 +1,35 @@
 defmodule Day9 do
-  def move_head(head_pos, "L") do
+  def move_head(head_pos, direction) do
     {x, y} = head_pos
-    {x - 1, y}
+
+    case direction do
+      "L" -> {x - 1, y}
+      "R" -> {x + 1, y}
+      "U" -> {x, y - 1}
+      "D" -> {x, y + 1}
+    end
   end
 
-  def move_head(head_pos, "R") do
-    {x, y} = head_pos
-    {x + 1, y}
+  def move_tail(head_pos, tail_pos) do
+    {head_x, head_y} = head_pos
+    {tail_x, tail_y} = tail_pos
+
+    case {tail_x - head_x, tail_y - head_y} do
+      {-2, -2} -> {head_x - 1, head_y - 1}
+      {-2, 2} -> {head_x - 1, head_y + 1}
+      {2, -2} -> {head_x + 1, head_y - 1}
+      {2, 2} -> {head_x + 1, head_y + 1}
+      {-2, _} -> {head_x - 1, head_y}
+      {2, _} -> {head_x + 1, head_y}
+      {_, -2} -> {head_x, head_y - 1}
+      {_, 2} -> {head_x, head_y + 1}
+      _ -> {tail_x, tail_y}
+    end
   end
-
-  def move_head(head_pos, "U") do
-    {x, y} = head_pos
-    {x, y - 1}
-  end
-
-  def move_head(head_pos, "D") do
-    {x, y} = head_pos
-    {x, y + 1}
-  end
-
-  defguardp is_positive(n) when n > 0
-  defguardp is_negative(n) when n < 0
-
-  def move_tail_x({head_x, _}, {tail_x, tail_y}) when head_x == tail_x,
-    do: IO.inspect({tail_x, tail_y}, label: "X equal")
-
-  def move_tail_x({head_x, _}, {tail_x, tail_y}) when abs(head_x - tail_x) < 2,
-    do: IO.inspect({tail_x, tail_y}, label: "X < 2 ")
-
-  def move_tail_x({head_x, _}, {tail_x, tail_y}) when is_positive(head_x - tail_x),
-    do: IO.inspect({tail_x + 1, tail_y}, label: "X positive")
-
-  def move_tail_x({head_x, _}, {tail_x, tail_y}) when is_negative(head_x - tail_x),
-    do: IO.inspect({tail_x - 1, tail_y}, label: "X positive")
-
-  def move_tail_y({_, head_y}, {tail_x, tail_y}) when head_y == tail_y,
-    do: IO.inspect({tail_x, tail_y}, label: "Y equal")
-
-  def move_tail_y({_, head_y}, {tail_x, tail_y}) when abs(head_y - tail_y) < 2,
-    do: IO.inspect({tail_x, tail_y}, label: "Y < 2")
-
-  def move_tail_y({_, head_y}, {tail_x, tail_y}) when is_positive(head_y - tail_y),
-    do: IO.inspect({tail_x, tail_y + 1}, label: "Y negative ")
-
-  def move_tail_y({_, head_y}, {tail_x, tail_y}) when is_negative(head_y - tail_y),
-    do: IO.inspect({tail_x, tail_y - 1}, label: "Y negative")
 
   def move(direction, head_pos, tail_pos) do
     new_head_pos = move_head(head_pos, direction)
-    IO.inspect(new_head_pos, label: "head")
-
-    new_tail_pos = move_tail_x(new_head_pos, tail_pos)
-    new_tail_pos = move_tail_y(new_head_pos, new_tail_pos)
-
-    IO.inspect(new_tail_pos, label: "tail")
+    new_tail_pos = move_tail(new_head_pos, tail_pos)
     {new_head_pos, new_tail_pos}
   end
 
@@ -65,9 +41,10 @@ defmodule Day9 do
     [[direction, distance] | tail] = movements
 
     {new_visited_cells, new_head_pos, new_tail_pos} =
-      Enum.reduce(0..distance, {visited_cells, head_pos, tail_pos}, fn _,
-                                                                       {visited_cells, head_pos,
-                                                                        tail_pos} ->
+      Enum.reduce(0..(distance - 1), {visited_cells, head_pos, tail_pos}, fn _,
+                                                                             {visited_cells,
+                                                                              head_pos,
+                                                                              tail_pos} ->
         {new_head_pos, new_tail_pos} = move(direction, head_pos, tail_pos)
         new_visited_cells = MapSet.put(visited_cells, new_tail_pos)
         {new_visited_cells, new_head_pos, new_tail_pos}
@@ -86,22 +63,8 @@ defmodule Day9 do
   end
 end
 
-# movements =
-#   File.read!('input.txt')
-#   |> String.split("\n")
-#   |> Enum.map(fn line -> String.split(line, " ") end)
-#   |> Enum.reject(fn x -> length(x) == 1 end)
-#   |> Enum.map(fn [direction, distance] -> [direction, String.to_integer(distance)] end)
-
 movements =
-  "R 4
-U 4
-L 3
-D 1
-R 4
-D 1
-L 5
-R 2"
+  File.read!('input.txt')
   |> String.split("\n")
   |> Enum.map(fn line -> String.split(line, " ") end)
   |> Enum.reject(fn x -> length(x) == 1 end)
