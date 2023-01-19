c3885c764bc084846e362882d3bf0fac78e3a03c
Henry Maddocks <hmaddocks@me.com>
2023-01-19 18:34:04 +1300

    Day 9 Elixir refactoring

--- day_9/day_9.exs
+++ day_9/day_9.exs
@@ -1,7 +1,5 @@
 defmodule Day9 do
-  def move_head(head_pos, direction) do
-    {x, y} = head_pos
-
+  def move_head({x, y}, direction) do
     case direction do
       "L" -> {x - 1, y}
       "R" -> {x + 1, y}
@@ -10,10 +8,7 @@ defmodule Day9 do
     end
   end
 
-  def move_tail(head_pos, tail_pos) do
-    {head_x, head_y} = head_pos
-    {tail_x, tail_y} = tail_pos
-
+  def move_tail({head_x, head_y}, {tail_x, tail_y}) do
     case {tail_x - head_x, tail_y - head_y} do
       {-2, -2} -> {head_x - 1, head_y - 1}
       {-2, 2} -> {head_x - 1, head_y + 1}
@@ -37,9 +32,7 @@ defmodule Day9 do
     visited_cells
   end
 
-  def apply_movements(visited_cells, movements, head_pos, tail_pos) do
-    [[direction, distance] | tail] = movements
-
+  def apply_movements(visited_cells, [[direction, distance] | tail], head_pos, tail_pos) do
     {new_visited_cells, new_head_pos, new_tail_pos} =
       Enum.reduce(0..(distance - 1), {visited_cells, head_pos, tail_pos}, fn _,
                                                                              {visited_cells,
@@ -59,7 +52,11 @@ defmodule Day9 do
     length(MapSet.to_list(visited_cells))
   end
 
-  def part_2 do
+  def part_2(movements) do
+    visited_cells = MapSet.new([{0, 0}])
+    knots = []
+    visited_cells = apply_movements(visited_cells, movements, {0, 0}, {0, 0})
+    length(MapSet.to_list(visited_cells))
   end
 end
 
@@ -73,5 +70,19 @@ movements =
 Day9.part_1(movements)
 |> IO.puts()
 
-Day9.part_2()
+movements2 =
+  "R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20"
+  |> String.split("\n")
+  |> Enum.map(fn line -> String.split(line, " ") end)
+  |> Enum.reject(fn x -> length(x) == 1 end)
+  |> Enum.map(fn [direction, distance] -> [direction, String.to_integer(distance)] end)
+
+Day9.part_2(movements2)
 |> IO.puts()
